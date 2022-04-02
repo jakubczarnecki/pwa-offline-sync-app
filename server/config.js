de19c177957bc95700d07da91fb8097b5f34d44a
jc <kczarnecok@wp.pl>
2022-04-02 14:25:09 +0200

    Init express server

---
+++ server/config.js
@@ -0,0 +1,3 @@
+const PORT = 8800;
+
+module.exports = PORT;
---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const PORT = require("./config");
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
