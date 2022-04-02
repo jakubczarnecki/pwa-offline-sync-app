1de11aa4357bd89e1ea7c82257712f40866c2f77
jc <kczarnecok@wp.pl>
2022-04-02 15:44:45 +0200

    Basic routing and mongoDB connection setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,30 @@
 const express = require("express");
-
-const PORT = require("./config");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+const PORT = require("./config");
+const notesRouter = require("./routers/notesRouter");
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+//routing
+app.use("/api/notes", notesRouter);
+
+//mongo database connection
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  () => {
+    console.log("Connected to MongoDB.");
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
---
+++ server/routers/notesRouter.js
@@ -0,0 +1,3 @@
+const notesRouter = require("express").Router();
+
+module.exports = notesRouter;
