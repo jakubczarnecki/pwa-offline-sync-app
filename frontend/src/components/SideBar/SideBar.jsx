093522ce517ade2e692af9939fcb8e814da9bde8
jc <kczarnecok@wp.pl>
2022-04-03 20:40:51 +0200

    Change sidebar design and add buttons

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import Button from "../shared/Button/Button";
 import IconButton from "../shared/IconButton/IconButton";
 import "./style.scss";
 
 const SideBar = ({ className, username }) => {
    return (
       <div className="side-bar">
-         <div className="greeter-container">Hello, {username}!</div>
+         <div className="greeter-container">
+            <div className="greeter-message section-title ">Hello, {username}!</div>
+         </div>
+         <div className="options-container">
+            <div className="options-container-top">
+               <Button className="fullwidth-button">Add note</Button>
+            </div>
+            <div className="options-container-bottom">
+               <Button className="fullwidth-button">Install on device</Button>
+               <Button className="fullwidth-button">Logout</Button>
+            </div>
+         </div>
       </div>
    );
 };
