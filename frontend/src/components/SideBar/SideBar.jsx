9667257d3bf06b1cbedcb9a77df161ab534df946
229935 <229935@edu.p.lodz.pl>
2022-04-26 01:01:10 +0200

    nie jest idealnie, ale jest lepiej

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -1,10 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import Button from "../shared/Button/Button";
+import IconButton from "../shared/IconButton/IconButton";
 import "./style.scss";
-import { dataContext } from "../../context/dataContext";
+import { dataContext, DataProvider } from "../../context/dataContext";
 
 const SideBar = ({ className, username, modal }) => {
-   const { dispatch } = useContext(dataContext);
+   const { state, dispatch } = useContext(dataContext);
+
+   const drawer = useRef(null);
+
+   const toggleDrawer = () => {
+      let currentClass = drawer.current.className;
+      drawer.current.className =
+         currentClass === "options-container hidden"
+            ? " options-container open"
+            : "options-container hidden";
+   };
 
    const handleLogout = (e) => {
       e.preventDefault();
@@ -15,11 +26,13 @@ const SideBar = ({ className, username, modal }) => {
    };
 
    return (
-      <div className={`side-bar ${className}`}>
+      <div className="side-bar">
          <div className="greeter-container">
-            <div className="greeter-message section-title ">Hello, {username}!</div>
+            <div className="greeter-message category-title ">Hello, {username}!</div>
+            <Button icon="menu" className="drawer-button" onClick={toggleDrawer}></Button>
          </div>
-         <div className="options-container">
+
+         <div ref={drawer} className="options-container hidden">
             <div className="options-container-top">
                <Button icon="add_circle" className="fullwidth-button" onClick={modal}>
                   Add note
