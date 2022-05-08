b239045c56f9c8aa5c60188345538746ef91b278
230026 Topolska Kamila <230026@edu.p.lodz.pl>
2022-05-08 20:46:24 +0200

    Add RoundButton, UseViewport, modify TopBar and BottomBar (resizing)

--- frontend/src/components/BottomBar/BottomBar.jsx
+++ frontend/src/components/BottomBar/BottomBar.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import Button from "../shared/Button/Button";
 import "./style.scss";
+import UseViewport from "../shared/UseViewport/UseViewport";
+import RoundButton from "../shared/RoundButton/RoundButton";
 
 const BottomBar = ({ className }) => {
+   const { width } = UseViewport();
+   const breakpoint = 900;
+
    return (
       <div className="bottom-bar">
          <h4>You can install our application on your device</h4>
-         <div className="buttons-wrapper">
-            <Button icon="download" onClick={() => {}}>
-               Install on device
-            </Button>
-            <Button icon="close" onClick={() => {}}>
-               Close
-            </Button>
-         </div>
+         {width > breakpoint ? (
+            <div className="buttons-wrapper">
+               <Button icon="download" onClick={() => {}}>
+                  Install on device
+               </Button>
+               <Button icon="close" onClick={() => {}}>
+                  Close
+               </Button>
+            </div>
+         ) : (
+            <div className="buttons-wrapper">
+               <RoundButton icon="download" onClick={() => {}} />
+               <RoundButton icon="close" onClick={() => {}} />
+            </div>
+         )}
       </div>
    );
 };
--- frontend/src/components/TopBar/TopBar.jsx
+++ frontend/src/components/TopBar/TopBar.jsx
@@ -1,14 +1,19 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useEffect, useState } from "react";
 import Button from "../shared/Button/Button";
 import IconButton from "../shared/IconButton/IconButton";
 import "./style.scss";
 import { dataContext, DataProvider } from "../../context/dataContext";
 import InstallButton from "../InstallButon/InstallButton";
+import UseViewport from "../shared/UseViewport/UseViewport";
+import RoundButton from "../shared/RoundButton/RoundButton";
 
 const TopBar = ({ className, username, modal }) => {
    const { state, dispatch } = useContext(dataContext);
 
    const drawer = useRef(null);
+
+   const { width } = UseViewport();
+   const breakpoint = 900;
 
    const toggleDrawer = () => {
       let currentClass = drawer.current.className;
@@ -37,14 +42,21 @@ const TopBar = ({ className, username, modal }) => {
    return (
       <div className="top-bar">
          <h4>Hello, {username}!</h4>
-         <div className="buttons-wrapper">
-            <Button icon="add_circle" onClick={handleModal}>
-               Add note
-            </Button>
-            <Button icon="logout" onClick={handleLogout}>
-               Logout
-            </Button>
-         </div>
+         {width > breakpoint ? (
+            <div className="buttons-wrapper">
+               <Button icon="add_circle" onClick={handleModal}>
+                  Add note
+               </Button>
+               <Button icon="logout" onClick={handleLogout}>
+                  Logout
+               </Button>
+            </div>
+         ) : (
+            <div className="buttons-wrapper">
+               <RoundButton icon="add_circle" onClick={handleModal} />
+               <RoundButton icon="logout" onClick={handleLogout} />
+            </div>
+         )}
       </div>
    );
 };
